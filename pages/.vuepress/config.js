67082345033431bf51b01b00f565a114307d1723
Yuze Men <me@sagirii.me>
2021-06-17 12:32:08 +0800

    Add Webfont.

--- pages/.vuepress/config.js
+++ pages/.vuepress/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  head: [["link", { rel: "icon", href: "/images/logo.png" }]],
+  head: [
+    ["link", { rel: "icon", href: "/images/logo.png" }],
+    ["script", { }, "(function(d){var config={kitId:'pzl5zkl',scriptTimeout:3000,async:true},h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,\"\")+\" wf-inactive\";},config.scriptTimeout),tk=d.createElement(\"script\"),f=false,s=d.getElementsByTagName(\"script\")[0],a;h.className+=\" wf-loading\";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!=\"complete\"&&a!=\"loaded\")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);"]],
   locales: {
     "/": {
       lang: "zh-CN",
